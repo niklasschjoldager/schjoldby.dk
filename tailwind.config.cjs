52b7c95a0209a8e8aadb02b7cad3f88745688777
Niklas Vejlby Schjoldager <61137786+niklasschjoldager@users.noreply.github.com>
2022-10-13 23:32:34 +0200

    Added projects section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html"],
   theme: {
     fontFamily: {
       sans: '"Source Sans Pro", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
     },
+
+    screens: {
+      xs: "480px",
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         primary: "#d396cf",
         secondary: "#032940",
-      },
-      screens: {
-        xs: "480px",
+        tertiary: "#fbf7ed",
       },
     },
   },
-  // corePlugins: {
-  //   fontSize: false,
-  // },
-  // plugins: [require("tailwindcss-fluid-type")],
 };
